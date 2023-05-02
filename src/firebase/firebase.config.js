8a68b9d0f58e6817a1455f5e5036da3daf0b6a2f
mohsin6862 <mohsinctg2206@gmail.com>
2023-05-02 14:33:32 +0600

    firebase added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCfrcu_QtDNfzW_EgxWs1UgbgP0xQV1VF4",
+  authDomain: "the-tasty-cook-43def.firebaseapp.com",
+  projectId: "the-tasty-cook-43def",
+  storageBucket: "the-tasty-cook-43def.appspot.com",
+  messagingSenderId: "454639515964",
+  appId: "1:454639515964:web:8cc2094928de3b89b64639"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
