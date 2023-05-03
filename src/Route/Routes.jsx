3d4b37ff8bfda5edde11d618985bbf6cf5db224b
mohsin6862 <mohsinctg2206@gmail.com>
2023-05-03 12:35:50 +0600

    Login Layout and routes added

--- src/Route/Routes.jsx
+++ src/Route/Routes.jsx
@@ -2,8 +2,21 @@ import Main from "../Layouts/Main";
 
 import { createBrowserRouter } from "react-router-dom";
 import Recipes from "../components/Recipe/Recipes";
+import Login from "../components/Login/Login";
+import LoginLayout from "../Layouts/LoginLayout/LoginLayout";
 
 const router =createBrowserRouter([
+    {
+        path:'/login',
+        element:<LoginLayout></LoginLayout>,
+        children:[
+            {
+                path:'/login',
+                element:<Login></Login>
+            }
+        ]
+
+    },
     {
         path:'/',
         element:<Main></Main>,
@@ -12,6 +25,9 @@ const router =createBrowserRouter([
                 path:'/recipes',
                 element:<Recipes></Recipes>,
                 loader:()=>fetch('http://localhost:5000/recipes')
+            },
+            {
+               
             }
         ]
       
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,69 +1,25 @@
 import React from 'react';
 import { Button, Carousel, Container, Form, Nav, Navbar } from 'react-bootstrap';
-import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+
 import { Link } from 'react-router-dom';
 import slider2 from '../../assets/n1.jpg'
 import slider3 from '../../assets/n2.jpg'
 import slider4 from '../../assets/pexels-pixabay-262978.jpg'
+import NavigationBar from '../../SharedComponents/NavigationBar';
 
 
 const Header = () => {
     return (
         <Container>
-            <div className='mt-5  d-flex align-items-center'>
-                <div className='flex-grow-1'>
-                    <p className='text-danger text-center fs-6 '> 22 QUICK AND EASY RECIPES IN 30 MINUTES (OR LESS) + 5 CHEF SECRETS TO MAKE YOU A BETTER COOK! </p>
-                </div>
-                <div className=''>
-                    <span className='fs-2 ms-2'><Link className='text-black'><FaFacebook></FaFacebook></Link></span>
-                    <span className='fs-2 ms-2'><Link className='text-black'><FaInstagram></FaInstagram></Link></span>
-                    <span className='fs-2 ms-2'><Link className='text-black'><FaTwitter></FaTwitter></Link></span>
-                </div>
-            </div>
-            <hr class="border border-secondary border-2 opacity-50 "></hr>
-            <h1 className='text-center  text-info my-2 size-xs '>The <span className='text-warning'>Tasty-Cook</span></h1>
 
-            {/* navbar started */}
-
-            <div>
-
-                <Navbar bg="light" expand="lg">
-                    <Container fluid>
-
-                        <Navbar.Collapse id="navbarScroll">
-                            <Nav
-                                className="me-auto my-2 my-lg-0 text-center"
-                                style={{ maxHeight: '100px' }}
-                                navbarScroll
-                            >
-                                <Nav.Link href="#action1">Home</Nav.Link>
-                                <Nav.Link href="#action2">Recipes</Nav.Link>
-                                <Nav.Link href="#action2">Blog</Nav.Link>
-                                <Nav.Link href="#action2">Login</Nav.Link>
-
-
-                            </Nav>
-                            <Form className="d-flex">
-                                <Form.Control
-                                    type="search"
-                                    placeholder="Search"
-                                    className="me-2"
-                                    aria-label="Search"
-                                />
-                                <Button variant="outline-success">Search</Button>
-                            </Form>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-
-            </div>
-
+            <NavigationBar></NavigationBar>
+           
             {/* banner */}
 
             <div className='my-5'>
                 <Carousel variant="dark">
                     <Carousel.Item>
-                        <img
+                        <img 
                             className="d-block w-100 "
                             src={slider2}
                             alt="First slide"
