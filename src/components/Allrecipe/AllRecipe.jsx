ff80b7d410c7000ccc0e0a47554d45e726780d31
mohsin6862 <mohsinctg2206@gmail.com>
2023-05-05 20:49:32 +0600

    add to favourite button added

--- src/components/Allrecipe/AllRecipe.jsx
+++ src/components/Allrecipe/AllRecipe.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
 import { Button, Card, CardGroup } from 'react-bootstrap';
 import { FaArrowAltCircleRight, FaArrowLeft, FaHeart } from 'react-icons/fa';
 import { Link, useLoaderData } from 'react-router-dom';
 
 const AllRecipe = () => {
+    const [addedToFavourites, setAddedToFavourites] = useState(false);
+    const handleAddToFavourites = () => {
+        setAddedToFavourites(true);
+        // You can add your code to save the favourite recipe here
+    }
     const allrecipe = useLoaderData()
     console.log(allrecipe)
     const { Recipe_image,Recipe_image1,Recipe_image2,recipeName,recipeName1,recipeName2,direction,ingredients1,ingredients2,ingredients3,ingredients4, rating } = allrecipe
     return (
         <div>
+            
             <CardGroup>
                 <Card>
                     <Card.Img style={{height:'200px'}} variant="top" src={Recipe_image} />
@@ -21,9 +28,10 @@ const AllRecipe = () => {
                       <p><FaArrowAltCircleRight></FaArrowAltCircleRight> {ingredients2}</p>
                       <p><FaArrowAltCircleRight></FaArrowAltCircleRight> {ingredients3}</p>
                       <p><FaArrowAltCircleRight></FaArrowAltCircleRight> {ingredients4}</p>
+                      <h6> Rating: {rating}</h6>
                         </Card.Text>
                      <div className='text-center'>
-                     <Button  className='mb-2' variant="secondary">ADD to Favourite <FaHeart></FaHeart></Button>
+                    
                     <Link to='/recipes'>  <Button  variant="secondary">Back to Home <FaArrowLeft></FaArrowLeft></Button></Link>
                      </div>
                     </Card.Body>
@@ -42,10 +50,11 @@ const AllRecipe = () => {
                       <p><FaArrowAltCircleRight></FaArrowAltCircleRight> {ingredients2}</p>
                       <p><FaArrowAltCircleRight></FaArrowAltCircleRight> {ingredients3}</p>
                       <p><FaArrowAltCircleRight></FaArrowAltCircleRight> {ingredients4}</p>
+                      <h6> Rating: {rating}</h6>
                    
                         </Card.Text>
                         <div className='text-center'>
-                     <Button  className='mb-2' variant="secondary">ADD to Favourite <FaHeart></FaHeart></Button>
+                    
                     <Link to='/recipes'>  <Button  variant="secondary">Back to Home <FaArrowLeft></FaArrowLeft></Button></Link>
                      </div>
                     </Card.Body>
@@ -64,21 +73,34 @@ const AllRecipe = () => {
                       <p><FaArrowAltCircleRight></FaArrowAltCircleRight> {ingredients2}</p>
                       <p><FaArrowAltCircleRight></FaArrowAltCircleRight> {ingredients3}</p>
                       <p><FaArrowAltCircleRight></FaArrowAltCircleRight> {ingredients4}</p>
+                      <h6> Rating: {rating}</h6>
  
                         </Card.Text>
                         <div className='text-center'>
-                     <Button  className='mb-2' variant="secondary">ADD to Favourite <FaHeart></FaHeart></Button>
+                   
                     <Link to='/recipes'>  <Button  variant="secondary">Back to Home <FaArrowLeft></FaArrowLeft></Button></Link>
                      </div>
                     </Card.Body>
                     <Card.Footer>
-                  <div className='d-flex'>
+                  <div className=''>
                   <small className="text-muted flex-grow-1">Watch this recipe on <Link>Youtube</Link></small>
-                  <p>Rating: {rating}</p>
+                 
                   </div>
                     </Card.Footer>
                 </Card>
             </CardGroup>
+            <div className='my-2 text-center'>
+            <Button
+                                className='mb-2'
+                                variant="warning"
+                                disabled={addedToFavourites}
+                                onClick={handleAddToFavourites}
+                            >
+                                {addedToFavourites ? "Added to Favourites" : "Add to Favourite"} 
+                                {addedToFavourites ? <FaHeart style={{marginLeft: "10px"}}/> : <FaHeart/>}
+                            </Button>
+            </div>
+           
         </div>
     );
 };
