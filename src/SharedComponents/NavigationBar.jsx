b54816fb63280760bedcae6c9581dec5419fe7e6
mohsin6862 <mohsinctg2206@gmail.com>
2023-05-05 15:57:43 +0600

    mobile responsive added

--- src/SharedComponents/NavigationBar.jsx
+++ src/SharedComponents/NavigationBar.jsx
@@ -10,73 +10,134 @@ const NavigationBar = () => {
         Logout()
     }
     return (
-        <Container>
-            <div className='mt-5  d-flex align-items-center'>
-                <div className='flex-grow-1'>
-                    <p className='text-danger text-center fs-6 '> 22 QUICK AND EASY RECIPES IN 30 MINUTES (OR LESS) + 5 CHEF SECRETS TO MAKE YOU A BETTER COOK! </p>
-                </div>
-                <div className=''>
-                    <span className='fs-2 ms-2'><Link className='text-black'><FaFacebook></FaFacebook></Link></span>
-                    <span className='fs-2 ms-2'><Link className='text-black'><FaInstagram></FaInstagram></Link></span>
-                    <span className='fs-2 ms-2'><Link className='text-black'><FaTwitter></FaTwitter></Link></span>
-                </div>
-            </div>
-            <hr class="border border-secondary border-2 opacity-50 "></hr>
-            <h1 className='text-center  text-info my-2 size-xs '>The <span className='text-warning'>Tasty-Cook</span></h1>
+        // <Container>
+        //     <div className='mt-5  d-flex align-items-center'>
+        //         <div className='flex-grow-1'>
+        //             <p className='text-danger text-center fs-6 '> 22 QUICK AND EASY RECIPES IN 30 MINUTES (OR LESS) + 5 CHEF SECRETS TO MAKE YOU A BETTER COOK! </p>
+        //         </div>
+        //         <div className=''>
+        //             <span className='fs-2 ms-2'><Link className='text-black'><FaFacebook></FaFacebook></Link></span>
+        //             <span className='fs-2 ms-2'><Link className='text-black'><FaInstagram></FaInstagram></Link></span>
+        //             <span className='fs-2 ms-2'><Link className='text-black'><FaTwitter></FaTwitter></Link></span>
+        //         </div>
+        //     </div>
+        //     <hr class="border border-secondary border-2 opacity-50 "></hr>
+        //     <h1 className='text-center  text-info my-2 size-xs '>The <span className='text-warning'>Tasty-Cook</span></h1>
 
-            {/* navbar started */}
+        //     {/* navbar started */}
 
-            <div>
+        //     <div>
 
-                <Navbar bg="light" expand="lg">
-                    <Container fluid>
+        //         <Navbar bg="light" expand="lg">
+        //             <Container fluid>
 
-                        <Navbar.Collapse id="navbarScroll">
-                            {/* <Nav
-                                className="me-auto my-2 my-lg-0 text-center"
-                                style={{ maxHeight: '100px' }}
-                                navbarScroll
-                            >
-                                <Nav.Link href="/" activeClassName="active">Home</Nav.Link>
-                                <Nav.Link href="#action2" activeClassName="active">Recipes</Nav.Link>
-                                <Nav.Link href="/blog" >Blog</Nav.Link>
+        //                 <Navbar.Collapse id="navbarScroll">
                            
+        //                     <Nav className="me-auto my-2 my-lg-0 text-center" style={{ maxHeight: '100px' }} navbarScroll>
+        //                         <NavLink exact to="/recipes" className="nav-link" activeClassName="active">
+        //                             Home
+        //                         </NavLink>
+        //                         <NavLink to="" className="nav-link" activeClassName="active">
+        //                             Recipes
+        //                         </NavLink>
+        //                         <NavLink to="/blog" className="nav-link" activeClassName="active">
+        //                             Blog
+        //                         </NavLink>
+        //                     </Nav>
+        //                     <Form className="d-flex">
+        //                         <Form.Control
+        //                             type="search"
+        //                             placeholder="Search"
+        //                             className="me-2"
+        //                             aria-label="Search"
+        //                         />
+        //                         <Button variant="outline-success">Search</Button>
+        //                     </Form>
+        //                     <Nav className=''>
+        //                         {user && <Nav.Link href="#deets"><Image title={user.displayName} style={{ width: '40px', height: '40px' }} src={user.photoURL} roundedCircle /></Nav.Link>}
+        //                         <Nav.Link eventKey={2} href="#memes">
+        //                             {user ? <Button variant="secondary" onClick={handleLogout}>Logout</Button> : <Link to='/login'><Button variant="secondary" >Login</Button></Link>}
+        //                         </Nav.Link>
+        //                     </Nav>
+        //                 </Navbar.Collapse>
+        //             </Container>
+        //         </Navbar>
 
+        //     </div>
 
-                            </Nav> */}
-                            <Nav className="me-auto my-2 my-lg-0 text-center" style={{ maxHeight: '100px' }} navbarScroll>
-                                <NavLink exact to="/recipes" className="nav-link" activeClassName="active">
-                                    Home
-                                </NavLink>
-                                <NavLink to="" className="nav-link" activeClassName="active">
-                                    Recipes
-                                </NavLink>
-                                <NavLink to="/blog" className="nav-link" activeClassName="active">
-                                    Blog
-                                </NavLink>
-                            </Nav>
-                            <Form className="d-flex">
-                                <Form.Control
-                                    type="search"
-                                    placeholder="Search"
-                                    className="me-2"
-                                    aria-label="Search"
-                                />
-                                <Button variant="outline-success">Search</Button>
-                            </Form>
-                            <Nav className=''>
-                                {user && <Nav.Link href="#deets"><Image title={user.displayName} style={{ width: '40px', height: '40px' }} src={user.photoURL} roundedCircle /></Nav.Link>}
-                                <Nav.Link eventKey={2} href="#memes">
-                                    {user ? <Button variant="secondary" onClick={handleLogout}>Logout</Button> : <Link to='/login'><Button variant="secondary" >Login</Button></Link>}
-                                </Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
+        // </Container>
+        <Container>
+  <div className='mt-5 d-flex flex-column flex-md-row align-items-center'>
+    <div className='flex-grow-1'>
+      <p className='text-danger text-center fs-6'>22 QUICK AND EASY RECIPES IN 30 MINUTES (OR LESS) + 5 CHEF SECRETS TO MAKE YOU A BETTER COOK! </p>
+    </div>
+    <div className='ms-md-3 mt-3 mt-md-0'>
+      <span className='fs-2 ms-2'><Link className='text-black'><FaFacebook/></Link></span>
+      <span className='fs-2 ms-2'><Link className='text-black'><FaInstagram/></Link></span>
+      <span className='fs-2 ms-2'><Link className='text-black'><FaTwitter/></Link></span>
+    </div>
+  </div>
+  <hr className='border border-secondary border-2 opacity-50'/>
+  <h1 className='text-center text-info my-2 size-xs'>The <span className='text-warning'>Tasty-Cook</span></h1>
 
-            </div>
+  {/* navbar started */}
 
-        </Container>
+  <div>
+
+    <Navbar bg='light' expand='lg'>
+      <Container fluid>
+
+        <Navbar.Toggle aria-controls='navbarScroll'/>
+        <Navbar.Collapse id='navbarScroll'>
+
+          <Nav className='me-auto my-2 my-lg-0 text-center' style={{ maxHeight: '100px' }} navbarScroll>
+            <NavLink exact to='/recipes' className='nav-link' activeClassName='active'>
+              Home
+            </NavLink>
+            <NavLink to='' className='nav-link' activeClassName='active'>
+              Recipes
+            </NavLink>
+            <NavLink to='/blog' className='nav-link' activeClassName='active'>
+              Blog
+            </NavLink>
+          </Nav>
+
+          <Form className='d-flex flex-column flex-md-row align-items-center ms-md-auto'>
+            <Form.Control
+              type='search'
+              placeholder='Search'
+              className='me-md-2 mb-3 mb-md-0'
+              aria-label='Search'
+            />
+            <Button variant='outline-success'>Search</Button>
+          </Form>
+
+          <Nav className='ms-md-3'>
+            {user && (
+              <Nav.Link href='#deets'>
+                <Image title={user.displayName} style={{ width: '40px', height: '40px' }} src={user.photoURL} roundedCircle/>
+              </Nav.Link>
+            )}
+            <Nav.Link eventKey={2} href='#memes'>
+              {user ? (
+                <Button variant='secondary' onClick={handleLogout}>
+                  Logout
+                </Button>
+              ) : (
+                <Link to='/login'>
+                  <Button variant='secondary'>Login</Button>
+                </Link>
+              )}
+            </Nav.Link>
+          </Nav>
+
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+  </div>
+</Container>
+
     );
 };
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,69 @@
+// import React from 'react';
+// import { Button, ListGroup } from 'react-bootstrap';
+// import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaNewspaper, FaTiktok, FaTwitter, FaYoutube } from 'react-icons/fa';
+// import { Link } from 'react-router-dom';
+
+// const Footer = () => {
+//     return (
+//         <div className="container bg-dark text-white mt-5">
+//             <div className="row">
+//                 <div className="col">
+//                     <h4 className='text-center  text-info my-2 size-xs mt-3'>The <span className='text-warning'>Tasty-Cook</span></h4>
+
+//                     <div className='ms-5 mt-5'>
+
+//                         <h6><Link>Home</Link></h6>
+//                         <h6><Link>About Us</Link></h6>
+//                         <h6><Link>Our Master Chef's</Link></h6>
+//                         <h6>   <Link>Blogs</Link></h6>
+//                         <h6> <Link>Recpies</Link></h6>
+
+
+
+
+
+//                     </div>
+
+//                 </div>
+//                 <div className="col ">
+//                 <h4 className='mt-5'>Terms and Condition</h4>
+//                 <div className='mt-5'>
+//                 <h6><Link>Privacy</Link></h6>
+//                         <h6><Link>Policy</Link></h6>
+//                         <h6><Link>Terms</Link></h6>
+//                         <h6>   <Link>Conditions</Link></h6>
+//                 </div>
+
+                   
+
+
+//                 </div>
+//                 <div className="col">
+//                 <h4 className='mt-5'>Subscribe Us</h4>
+//                     <p><Button className='mt-5' variant="outline-primary"> <FaYoutube /> Youtube</Button></p>
+//                     <p>  <Button variant="outline-primary"><FaTiktok />  TIK-TOK</Button></p>
+//                     <p>    <Button variant="outline-primary"><FaNewspaper /> News-Letter</Button></p>
+                    
+                    
+//                 </div>
+//                 <div className="col">
+//                 <h4 className='mt-5'>Follow Us</h4>
+//                     <p><Button className='mt-5' variant="outline-primary"> <FaFacebook /> Facebook</Button></p>
+//                     <p>  <Button variant="outline-primary"><FaTwitter />  Twitter</Button></p>
+//                     <p>    <Button variant="outline-primary"><FaInstagram /> Instagram</Button></p>
+//                 </div>
+//             </div>
+//             <div className="row">
+//                 <div className="col-12">
+//                     <h6 className='text-center my-4'>All copyright reserved for The Testy-cook</h6 >
+//                 </div>
+               
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Footer;
 import React from 'react';
 import { Button, ListGroup } from 'react-bootstrap';
 import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaNewspaper, FaTiktok, FaTwitter, FaYoutube } from 'react-icons/fa';
@@ -7,47 +73,33 @@ const Footer = () => {
     return (
         <div className="container bg-dark text-white mt-5">
             <div className="row">
-                <div className="col">
+                <div className="col-12 col-md-4">
                     <h4 className='text-center  text-info my-2 size-xs mt-3'>The <span className='text-warning'>Tasty-Cook</span></h4>
-
                     <div className='ms-5 mt-5'>
-
                         <h6><Link>Home</Link></h6>
                         <h6><Link>About Us</Link></h6>
                         <h6><Link>Our Master Chef's</Link></h6>
-                        <h6>   <Link>Blogs</Link></h6>
-                        <h6> <Link>Recpies</Link></h6>
-
-
-
-
-
+                        <h6><Link>Blogs</Link></h6>
+                        <h6><Link>Recpies</Link></h6>
                     </div>
-
                 </div>
-                <div className="col ">
-                <h4 className='mt-5'>Terms and Condition</h4>
-                <div className='mt-5'>
-                <h6><Link>Privacy</Link></h6>
+                <div className="col-12 col-md-4">
+                    <h4 className='mt-5'>Terms and Condition</h4>
+                    <div className='mt-5'>
+                        <h6><Link>Privacy</Link></h6>
                         <h6><Link>Policy</Link></h6>
                         <h6><Link>Terms</Link></h6>
-                        <h6>   <Link>Conditions</Link></h6>
+                        <h6><Link>Conditions</Link></h6>
+                    </div>
                 </div>
-
-                   
-
-
-                </div>
-                <div className="col">
-                <h4 className='mt-5'>Subscribe Us</h4>
+                <div className="col-12 col-md-2">
+                    <h4 className='mt-5'>Subscribe Us</h4>
                     <p><Button className='mt-5' variant="outline-primary"> <FaYoutube /> Youtube</Button></p>
                     <p>  <Button variant="outline-primary"><FaTiktok />  TIK-TOK</Button></p>
                     <p>    <Button variant="outline-primary"><FaNewspaper /> News-Letter</Button></p>
-                    
-                    
                 </div>
-                <div className="col">
-                <h4 className='mt-5'>Follow Us</h4>
+                <div className="col-12 col-md-2">
+                    <h4 className='mt-5'>Follow Us</h4>
                     <p><Button className='mt-5' variant="outline-primary"> <FaFacebook /> Facebook</Button></p>
                     <p>  <Button variant="outline-primary"><FaTwitter />  Twitter</Button></p>
                     <p>    <Button variant="outline-primary"><FaInstagram /> Instagram</Button></p>
@@ -57,7 +109,6 @@ const Footer = () => {
                 <div className="col-12">
                     <h6 className='text-center my-4'>All copyright reserved for The Testy-cook</h6 >
                 </div>
-               
             </div>
         </div>
     );
